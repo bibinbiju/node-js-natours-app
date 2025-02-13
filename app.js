11b727a32b4dd8fcb8fa633e32aa640ac4d0fb77
Bibin Biju <bibinvakkom@gmail.com>
2025-02-13 11:20:02 +0530

    Added parameter pollution middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -39,7 +40,21 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
-// Serving static files
+// Prevent paramater pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficutlty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files 
 app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
